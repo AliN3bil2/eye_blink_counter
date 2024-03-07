dc88fa4b115998024e3e79503dcd4e0fc719964c
AliN3bil2 <134061031+AliN3bil2@users.noreply.github.com>
2024-03-07 13:48:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,48 @@ from detector.mediapipe_detector import MediapipeDetector
 import warnings
 
 if __name__ == "__main__":
-    detector = MediapipeDetector()
-    cap = cv2.VideoCapture(r"WIN_20231220_17_58_13_Pro.mp4")
+    detect = MediapipeDetector()
+    cap = cv2.VideoCapture(r"G:\data_for_drawssy\IMG_2597.MOV")
+    
     while True:
-        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
-            #cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        #frame_number = int(cap.get(cv2.CAP_PROP_POS_FRAMES))
+        if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):                
+            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
             break
         success, img = cap.read()
         if success:
-            img, left_eye_state, right_eye_state = detector.detect(img, visualise=True)
-            print(left_eye_state)
-            cv2.imshow('img',img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+            #img, left_eye_state, right_eye_state, detected_face, store = detect.detect(img, visualise=True)
+            img, left_eye_state, right_eye_state, detected_face = detect.detect(img, visualise=True)
 
+            if detected_face is not None :
+                num_landmarks = len(detected_face.landmark)
+                max_landmark_index = max(max(detect.triangle_points), max(detect.left_eye_id_list), max(detect.right_eye_id_list))
+                if num_landmarks >= max_landmark_index + 1:
+                     angle = detect.fraction(detected_face, detect.triangle_points)
+                     mouth_ratio = detect.mouth(detected_face, detect.mouth_list)
+                     mouth_state = detect.detect_mouth_state(mouth_ratio, detect.ywan)
+                     mouth_state_message = "ywan" if mouth_state == detect.ywan else "active"
+                     blink_count = detect.detect_blinks(detect.previous_eye_state,detect.left_eye_state, detect.right_eye_state)
+                     
+                     cv2.putText(img, mouth_state_message, (10,80 ), cv2.FONT_HERSHEY_SIMPLEX, 1, (150, 0, 20), 2)
+                     print("Left_eye:", left_eye_state, "Right_eye:", right_eye_state, "Angle:", angle, "mouth_state", mouth_state  )
+                  
+                     
+                    
+            else:
+                print("No face detected or no landmarks found.")
+        
+            #print(detect.divided_ratio)  
+                
+            cv2.imshow('img', img)
+            if cv2.waitKey(1) & 0xFF == ord('s'):
+                break
         else:
-            warnings.warn("No image found !!")
-    
+            warnings.warn("No image found !!")  
+    print(blink_count)    
     cap.release()
+    cv2.destroyAllWindows()
+
     cv2.destroyAllWindows()
 
 
